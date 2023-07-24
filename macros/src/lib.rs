b1b1553aabd96312e7bb16b5c612918fe727171d
matteo.ielacqua <matteo.ielacqua@inpeco.com>
2023-07-24 17:16:39 +0200

    add macro files

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,6 +1,35 @@
-use proc_macro::TokenStream;
+mod support;
+use proc_macro::{TokenStream, Group};
+use quote::quote;
 
-#[proc_macro_derive(LwItem)]
+#[proc_macro_derive(LwItem,attributes(Header))]
 pub fn lwitem_macro(input : TokenStream) -> TokenStream{
-      TokenStream::new()
+      let name = "";
+      let field = "";
+      for v in input.into_iter(){
+            match v {
+                proc_macro::TokenTree::Group(grp) => {lwitem_macro(grp.stream());},
+                proc_macro::TokenTree::Ident(id) => println!("id: {}",id),
+                proc_macro::TokenTree::Punct(punc) => println!("punc: {}",punc),
+                proc_macro::TokenTree::Literal(lit) => println!("lit: {}",lit),
+            }
+      }
+      /* 
+     let token = quote!(
+            impl LwItem for #name{
+                  pub fn get_header(&self) -> LwHeader{
+                        self.#field
+                  }
+            }
+      );
+
+      return token.into();
+      */
+      TokenStream::new()     
 }
+
+#[proc_macro_attribute]
+pub fn lwitem_attr(input : TokenStream, field: TokenStream) -> TokenStream{
+      todo!()
+}
+
--- src/Events.rs
+++ src/Events.rs
@@ -1,42 +1,60 @@
 use core::fmt;
 
+use NESLib_macro::LwItem;
 
-#[derive(Clone,Copy)]
-pub enum EventType{
-      ARRIVAL,
-      DEPARTURE,
-      END,
-      PROBE,
-      MAINTENANCE,
-      NOEVENT
+use crate::Collections::LightweightList::LwHeader;
+
+#[derive(Clone, Copy)]
+pub enum EventType {
+    ARRIVAL,
+    DEPARTURE,
+    END,
+    PROBE,
+    MAINTENANCE,
+    NOEVENT,
 }
 
-#[derive(Clone,Copy)]
-pub struct Event{
-      pub kind : EventType,
-      pub createTime: f64,
-      pub occurTime: f64,
-      pub serviceTime: f64,
-      pub arrivalTime: f64,
-      pub subType: EventType
+#[derive(LwItem)]
+pub struct Event {
+   #[Header] _header: LwHeader,
+    pub kind: EventType,
+    pub createTime: f64,
+    pub occurTime: f64,
+    pub serviceTime: f64,
+    pub arrivalTime: f64,
+    pub subType: EventType,
 }
 
-impl Event{
-      pub fn new(kind: EventType,createTime: f64, occurTime: f64, serviceTime:f64, arrivalTime:f64) -> Self{
-            Event { kind, createTime, occurTime, serviceTime, arrivalTime, subType: EventType::NOEVENT }
-      }      
+impl Event {
+    pub fn new(
+        kind: EventType,
+        createTime: f64,
+        occurTime: f64,
+        serviceTime: f64,
+        arrivalTime: f64,
+    ) -> Self {
+        Event {
+            _header: LwHeader::new(),
+            kind,
+            createTime,
+            occurTime,
+            serviceTime,
+            arrivalTime,
+            subType: EventType::NOEVENT,
+        }
+    }
 }
 
-impl fmt::Display for EventType{
+impl fmt::Display for EventType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-      let mut result : &str = "";
+        let mut result: &str = "";
         match self {
             EventType::ARRIVAL => result = "Arrival",
-            EventType::DEPARTURE => result="Departure",
-            EventType::END => result="End",
-            EventType::PROBE => result= "Probe",
-            EventType::MAINTENANCE => result="Maintenance",
-            EventType::NOEVENT => result="NoEvent",
+            EventType::DEPARTURE => result = "Departure",
+            EventType::END => result = "End",
+            EventType::PROBE => result = "Probe",
+            EventType::MAINTENANCE => result = "Maintenance",
+            EventType::NOEVENT => result = "NoEvent",
         }
         f.write_str(result)
     }
